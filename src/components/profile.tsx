e400be2d75582877cb9e7edaa9557bc89a51f37b
lyaepso <lyaepso@gmail.com>
2021-02-23 21:35:03 -0300

    dia 2 NLW4 em andamento

---
+++ src/components/profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/lyaepso.png" alt='Foto de Eliane na Praia com seu esposo'/>
+            
+                <strong>Eliane Oliveira</strong>
+                <p>Level 1</p>
+        </div>
+        
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/profile";
+
+
+export default function Home() {
+  return (
+    <div className="container">
+     <ExperienceBar />
+
+     <section>
+       <div>
+        <Profile />
+       </div>
+       <div>
+
+       </div>
+     </section>
+    </div>
+  )
+}
