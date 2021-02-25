9264302d6fc7599afb4f40a63c4587709b36af56
lyaepso <lyaepso@gmail.com>
2021-02-25 17:26:34 -0300

    .

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,14 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from "../styles/components/Countdown.module.css";
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext);
+
     const [time, setTime] = useState(0.1*60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
+
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
+
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
@@ -16,13 +21,13 @@ export function Countdown() {
         setIsActive(true);
     }
 
-    function resetCountdown() {
+    function resetCountdown()  {
         clearTimeout(countdownTimeout);
         setIsActive(false);
         setTime(0.1*60);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
        if (isActive && time > 0) {
         countdownTimeout = setTimeout(() => {
                setTime(time -1);
@@ -30,6 +35,7 @@ export function Countdown() {
         } else if (isActive && time == 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
        
     }, [isActive, time])
